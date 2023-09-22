d3a82fca7d186a1bb0edf3220e873f2e692951b0
linich <jorge_soto_1999@hotmail.com>
2023-09-22 08:37:08 -0300

    backend basico para registro

--- server.js
+++ server.js
@@ -4,12 +4,33 @@ const cors = require('cors')
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "pawtitas"
+})
+
+app.post('/Register',(req,res) => {
+    const sql = "INSERT INTO usuarios (`RUT`,`Nombre`,`Apellidos`,`Correo`,`Contraseñas`) VALUES (?)";
+    const values = [
+        req.body.rut,
+        req.body.nombre,
+        req.body.apellidos,
+        req.body.email,
+        req.body.contraseña
+    ]
+    
+    db.query(sql,[values], (err,data)=>{
+        if(err){
+            return res.json("Error");
+        }
+        console.log(values.req.body.rut)
+        return res.json(data);
+        
+    })
 })
 
 app.listen(8081, ()=> {
