1da50f6b3888a1b5d29d36242e4bd1fc72873c52
brazann <bzapata2002@gmail.com>
2023-10-18 20:11:33 -0300

    Mis Mascotas BD, Formulario de agregar
    Se creo una collecion dentro de la tabla usuarios para que cada usuario tenga su tabla mis mascotas, al agregar una mascota se crea esta collecion

--- src/pages/AñadirMisMascotas.jsx
+++ src/pages/AñadirMisMascotas.jsx
@@ -1,92 +1,89 @@
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import "../components/css/UserProfile.css";
+import React, { useState } from 'react';
+import { UserAuth } from '../components/Autenticacion';
+import { doc, collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function AñadirMisMascotas({ agregarMascota, onCancel }) {
-  // Estado para almacenar los datos de la nueva mascota
-  const [nuevaMascota, setNuevaMascota] = useState({
-    nombre: "",
-    raza: "",
-    género: "",
-    historia: "",
-    edad: "",
-    peso: "",
-  });
+  const { user } = UserAuth();
 
-  // Función para manejar la adición de una nueva mascota
-  const manejarAgregarMascota = () => {
-    // Verifica si todos los campos están completos antes de agregar la mascota
-    if (nuevaMascota.nombre && nuevaMascota.raza && nuevaMascota.género && nuevaMascota.edad && nuevaMascota.peso && nuevaMascota.historia) {
-      // Llama a la función de agregarMascota y reinicia el estado de nuevaMascota
-      agregarMascota(nuevaMascota);
-      setNuevaMascota({
-        nombre: "",
-        raza: "",
-        género: "",
-        historia: "",
-        edad: "",
-        peso: "",
+  const [nombre, setNombre] = useState('');
+  const [tipo, setTipo] = useState('');
+  const [raza, setRaza] = useState('');
+  const [sexo, setSexo] = useState('');
+  const [edad, setEdad] = useState('');
+  const [historia, setHistoria] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const manejarAñadirMascota = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Obtener la referencia del documento del usuario
+      const usuarioRef = doc(db, 'Usuarios', user.uid);
+      // Obtener la referencia de la colección "MisMascotas" dentro del documento del usuario
+      const misMascotasRef = collection(usuarioRef, 'MisMascotas');
+
+      // Añadir la nueva mascota a la colección "MisMascotas"
+      await addDoc(misMascotasRef, {
+        NombreMascota: nombre,
+        TipoMascota: tipo,
+        RazaMascota: raza,
+        SexoMascota: sexo,
+        EdadMascota: parseInt(edad),
+        HistoriaMascota: historia,
+        ImagenMascota: imagen,
       });
-    } else {
-      // Muestra una alerta si no todos los campos están completos
-      alert("Por favor, complete todos los campos.");
+
+      // Limpiar el formulario después de agregar la mascota
+      setNombre('');
+      setTipo('');
+      setRaza('');
+      setSexo('');
+      setEdad('');
+      setHistoria('');
+      setImagen('');
+
+      alert('Mascota añadida exitosamente.');
+    } catch (error) {
+      console.error('Error al añadir la mascota a MisMascotas:', error);
     }
   };
 
-  // Renderiza el formulario para agregar una nueva mascota con campos de entrada y botones de acción
+  if (!user) {
+    return <div>No estás autenticado.</div>;
+  }
+
   return (
-    <div className="formulario-agregar-mascota">
-      <h4>Agregar Nueva Mascota</h4>
-      <input
-        type="text"
-        placeholder="Nombre"
-        value={nuevaMascota.nombre}
-        onChange={(e) => setNuevaMascota({ ...nuevaMascota, nombre: e.target.value })}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Raza"
-        value={nuevaMascota.raza}
-        onChange={(e) => setNuevaMascota({ ...nuevaMascota, raza: e.target.value })}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Género"
-        value={nuevaMascota.género}
-        onChange={(e) => setNuevaMascota({ ...nuevaMascota, género: e.target.value })}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Historia"
-        value={nuevaMascota.historia}
-        onChange={(e) => setNuevaMascota({ ...nuevaMascota, historia: e.target.value })}
-      />
-      <input
-        type="text"
-        placeholder="Edad"
-        value={nuevaMascota.edad}
-        onChange={(e) => setNuevaMascota({ ...nuevaMascota, edad: e.target.value })}
-      />
-      <input
-        type="text"
-        placeholder="Peso"
-        value={nuevaMascota.peso}
-        onChange={(e) => setNuevaMascota({ ...nuevaMascota, peso: e.target.value })}
-      />
-      {/* Botón para cancelar la operación */}
-      <Button variant="danger" onClick={onCancel}>
-        Cancelar
-      </Button>
-      {/* Botón para agregar la nueva mascota */}
-      <Button variant="success" onClick={manejarAgregarMascota}>
-        Agregar Mascota
-      </Button>
+    <div className='formulario-agregar-mascota'>
+      <h2>Añadir Nueva Mascota</h2>
+      <form onSubmit={manejarAñadirMascota}>
+        <label htmlFor="nombre">Nombre de la Mascota:</label>
+        <input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+
+        <label htmlFor="tipo">Tipo de Mascota:</label>
+        <input type="text" id="tipo" value={tipo} onChange={(e) => setTipo(e.target.value)} required />
+
+        <label htmlFor="raza">Raza de la Mascota:</label>
+        <input type="text" id="raza" value={raza} onChange={(e) => setRaza(e.target.value)} required />
+
+        <label htmlFor="sexo">Sexo de la Mascota:</label>
+        <input type="text" id="sexo" value={sexo} onChange={(e) => setSexo(e.target.value)} required />
+
+        <label htmlFor="edad">Edad de la Mascota:</label>
+        <input type="number" id="edad" value={edad} onChange={(e) => setEdad(e.target.value)} required />
+
+        <label htmlFor="historia">Historia de la Mascota:</label>
+        <textarea id="historia" value={historia} onChange={(e) => setHistoria(e.target.value)} required />
+
+        <label htmlFor="imagen">URL de la Imagen de la Mascota:</label>
+        <input type="text" id="imagen" value={imagen} onChange={(e) => setImagen(e.target.value)} required />
+
+        <button type="button" onClick={onCancel}>Volver</button>
+
+        <button type="submit">Añadir Mascota</button>
+      </form>
     </div>
   );
 }
 
-// Exporta el componente AñadirMisMascotas
 export default AñadirMisMascotas;
