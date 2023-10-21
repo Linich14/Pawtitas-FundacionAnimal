f5886292a4dc582b37fe4de8a62dc84c6c05a28c
Sebastián <sebastian.munoz2020@alu.uct.cl>
2023-10-20 22:41:58 -0300

    cambio a funcion de subir imagenes a db
    ahora las imágenes si se guardan dentro de una carpeta especifica

--- src/components/FuncionesPaginaGaleria.js
+++ src/components/FuncionesPaginaGaleria.js
@@ -4,8 +4,19 @@ import {uploadBytes,ref } from '@firebase/storage'
 
 
 export function uploadFile(file) {
-    const StorageRef = ref (storage, v4())
+    // const StorageRef = ref (storage, v4())
+    const StorageRef = ref(storage, 'ImagenesGaleria/' + v4() + '/' + file.name);
     uploadBytes(StorageRef, file).then(snapshot => {
       console.log(snapshot)
     })
   }    
+
+// const storageRef = ref(storage, 'imagenesGaleria/' + v4() + '/' + file.name);
+
+//   // Sube el archivo a la carpeta especificada
+//   uploadBytes(storageRef, file).then(snapshot => {
+//     console.log('Archivo subido con éxito:', snapshot);
+//   }).catch(error => {
+//     console.error('Error al subir el archivo:', error);
+//   });
+// }
