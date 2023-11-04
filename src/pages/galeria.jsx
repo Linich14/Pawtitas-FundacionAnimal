ce9e4997bc21905ccd119d2a6335a9a0dddd92e2
Sebastián <sebastian.munoz2020@alu.uct.cl>
2023-11-03 21:04:18 -0300

    agregado a galeria de mostrar imagenes
    se pueden subir imágenes y datos a la bd además de que se muestran en la pagina(de manera muy rustica por ahora)

--- src/pages/galeria.jsx
+++ src/pages/galeria.jsx
@@ -6,7 +6,14 @@ import Footer from '../components/Footer'
 
 // ------------------------------------------------------------------------------------------------
 //cosas para subir imagenes a la db
-import { uploadFile } from '../components/FuncionesPaginaGaleria'
+import {uploadBytes,ref,getDownloadURL } from '@firebase/storage'
+import { storage } from '../firebase'
+import { db } from '../firebase'
+import { v4 } from "uuid";
+
+import { addDoc, collection, getDoc, getDocs } from '@firebase/firestore'
+
+// import { uploadFile } from '../components/FuncionesPaginaGaleria'
 // import { uploadFile } from '../firebase'
 import { useState } from 'react'
 
@@ -16,6 +23,7 @@ import Cards from "../components/Cards";//componente que contiene la estructura 
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { useEffect } from 'react'
 
 
 
@@ -23,12 +31,40 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 function Galeria() {
 // ------------------------------------------------------------------------------------------------
 //cosas para subir imagenes a la db
-  const [file, setFile] = useState(null)
+  const [txt,setTxT] = useState('') //usada para subir texto
+  const [file,setFile] = useState('') // para la imagen
+  const [data,setData] = useState([]) //para llamar a la bd y mostrar
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    uploadFile(file);
+  const handleSubmit = (e) => { //funcion para subir los datos a la bd
+    console.log(e.target.files[0])
+    const imgs = ref(storage, 'ImagenesGaleria/' + v4()) //dictamos la ruta donde queremos que se suba la foto
+    uploadBytes(imgs,e.target.files[0]).then(data=>{// la subimo con esta funcion
+      console.log(data,"imgs")
+      getDownloadURL(data.ref).then(val=>{//con esto conseguimos el url de la imagen subida
+        setFile(val)//aqui se almacena
+      })
+    })
+
   }
+// configuracion para el boton del formulario de subir a galeria
+  const handleClick = async () =>{ //aqui es para el boton de subir a la bd
+    const valRef = collection(db,'Galeria')//la coleccion a la que se debe subir 
+    await addDoc(valRef,{txtVal:txt,fileURL:file})//con esto enlazamos la imagen en storage y el texto en firebase 
+    alert("¡Datos guados de manera exitosa!") //avisamos que los datos han sido subidos 
+  }
+
+  const getData = async ()=>{//con esto conseguimos la info de la bd
+    const valRef = collection(db,'Galeria')//firebase       AQUI CONSEGUIMOS LOS DATOS
+    const dataDB = await getDocs(valRef)//storage
+    const allData = dataDB.docs.map(val=>({...val.data(),id:val.id}))//hacemos el mapeo y seleccionamos
+    //lo que queremos ver y acceder
+    setData(allData)
+    console.log(dataDB)
+  }
+
+  useEffect(()=>{// aqui es como llamamos a los datos para que puedan verse mediante el useEffect
+    getData()
+  })
 
 //-------------------------------------------------------------------------------------------------
   return (
@@ -47,30 +83,33 @@ function Galeria() {
     </div>
 
 
-
 {/* aqui ira el epacio para subir una archivo y crear una opinion */}
 
     <div className='galeriabackground'>
     <div className='container col-md-10 mx-auto col-lg-12'>
 
-    <form onSubmit={handleSubmit}>
-      <input type="file" className='' id='' onChange={(e) => setFile(e.target.files[0])} /> 
-      <button> Subir </button>
-    </form>
+    <div>
+        <h1>¿Quieres ver a tu mascota a ésta galeria?</h1>
+        <input onChange={(e) => setTxT(e.target.value)} placeholder='Agregar texto'/><br/>
+        <input type='file' onChange={(e) => handleSubmit(e)}/><br/>
+        <button onClick={handleClick}>Subir a galeria</button>
 
+
+        {//en esta seccion se muentran los datos subidos
+        // se hace el mapeo
+          data.map(value=> <div> 
+              <h1>{value.txtVal}</h1> {/* se llama al texto subido */}
+              <img src={value.fileURL} height='200px' width='200px'/>  {/* se llama a la imagen */}
+          </div>)
+        }
+    </div>
 
     </div>
     </div>
 {/* termino de espacio para dar opinion */}
 
 
-
-
-
-
-
-
-    {/* aqui creamos el grupo de botones */}
+{/* aqui creamos el grupo de botones */}
     <div className="mis-botones-grupo-css d-flex justify-content-center align-items-center">
       <ButtonToolbar aria-label="Toolbar with button groups">
         <ButtonGroup className="me-2" aria-label="First group">
